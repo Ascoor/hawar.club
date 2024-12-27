28622b8a7c2de22ea79541f2caa6bbd248ed4646
askar <mr.askar1984@gmail.com>
2024-12-27 07:41:00 +0200

    sdhkssj

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'gh-pages' ? '/hawar.club/' : '/', // ضبط المسار الأساسي بناءً على البيئة
   optimizeDeps: {
     include: ['swiper'], // إذا كنت تستخدم مكتبات إضافية مثل Swiper
   },
   server: {
-    port: 3000,  // إعداد المنفذ
-    open: true,  // فتح المتصفح تلقائيًا عند بدء الخادم
+    port: 3000, // إعداد المنفذ
+    open: true, // فتح المتصفح تلقائيًا عند بدء الخادم
   },
   build: {
     chunkSizeWarningLimit: 1000,
-    outDir: 'dist',  
-    minify: 'esbuild',  // تحسين الكود لتقليل الحجم
-    sourcemap: true,  // إنشاء خريطة المصادر
+    outDir: 'dist',
+    minify: 'esbuild', // تحسين الكود لتقليل الحجم
+    sourcemap: true, // إنشاء خريطة المصادر
   },
-});
+}));
